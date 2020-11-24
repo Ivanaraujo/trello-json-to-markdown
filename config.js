1bf9c3ea47a73dd461c50c2e1e70f3a9c8f682dd
unknown <vantrix@gmail.com>
2020-11-24 12:32:38 -0300

    config testing

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 //Replace 'your_key' with your key
-module.exports.key = 'your_key';
+module.exports.key = 'key';
 
 //Replace 'your_token' with your application's token
-module.exports.token = 'your_token';
+module.exports.token = 'token';
 
 //Put all of the desired boards' IDs into this array
 module.exports.boards = ['boardID1', 'boardID2'];
+
